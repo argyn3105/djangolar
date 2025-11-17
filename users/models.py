b744170f2451849f69828223d1632a9fc702d5b2
argyn3105 <arg_amantay@kbtu.kz>
2025-11-17 22:39:33 +0500

    lab7

--- users/models.py
+++ users/models.py
@@ -1,31 +1,66 @@
-
 from django.db import models
-from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
 
-class CustomUser(AbstractUser):
-    """User model extending AbstractUser"""
-    middle_name = models.CharField(max_length=50, blank=True, null=True)
-    birth_date = models.DateField(blank=True, null=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    phone = models.CharField(max_length=30, blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True, null=True)
+    country = models.CharField(max_length=30, blank=True, null=True)
+
+    department = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        choices=[
+            ("IT", "IT"),
+            ("HR", "HR"),
+            ("Sales", "Sales"),
+            ("Finance", "Finance"),
+        ],
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ("admin", "admin"),
+            ("manager", "manager"),
+            ("employee", "employee"),
+            
+        ],
+        default="employee"
+    )
+
+    birth_date = models.DateField(null=True, blank=True)
+    salary = models.IntegerField(null=True, blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['']
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.username
-
-
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-# from django.utils import timezone
-
-# class CustomAccount(AbstractBaseUser, PermissionsMixin):
-#     """User model using AbstractBaseUser"""
-#     email = models.EmailField(unique=True)
-#     full_name = models.CharField(max_length=150)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#     date_joined = models.DateTimeField(default=timezone.now)
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['full_name']
-
-#     def __str__(self):
-#         return self.email
+        return self.email
